26359bce7aa1bcdeddda71ccd13f2d10e6c319e5
M Fajrul Khaq <muhammad.fajrul@lintasarta.co.id>
2026-09-16 10:41:02 +0700

    feat: setup struktur folder project and setup db

---
+++ src/main/java/fajrull/takehomeporject/TakehomeporjectApplication.java
@@ -0,0 +1,13 @@
+package fajrull.takehomeporject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakehomeporjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakehomeporjectApplication.class, args);
+	}
+
+}
